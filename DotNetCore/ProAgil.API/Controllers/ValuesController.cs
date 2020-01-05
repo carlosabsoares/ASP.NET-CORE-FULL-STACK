e9e6292deaaa0f626dbdc3dcdf13c6c776579ab1
carlosabsoares <carlosabsoareso@gmaill.com>
2020-01-05 19:47:47 -0300

    Aula EF

--- DotNetCore/ProAgil.API/Controllers/ValuesController.cs
+++ DotNetCore/ProAgil.API/Controllers/ValuesController.cs
@@ -4,101 +4,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProAgil.API.Model;
+using ProAgil.API.Data;
 
-namespace ProAgil.API.Controllers
-{
-    [Route("api/[controller]")]
+namespace ProAgil.API.Controllers {
+    [Route ("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
-    {
+    public class ValuesController : ControllerBase {
+        public readonly DataContext _context ;
+
+        public ValuesController (DataContext context) {
+            _context = context;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<Evento>> Get()
-        {
-            return new Evento[]
-            {
-                new Evento()
-                {
-                    EventoId = 1,
-                    Tema = "Angular e .NET Core",
-                    Local = "Belo Horizonte",
-                    Lote = "1º Lote",
-                    QtdPessoas= 250,
-                    DataEvento = DateTime.Now.AddDays(2) .ToString("dd/MM/yyyy")
-                },
-                new Evento()
-                {
-                    EventoId = 2,
-                    Tema = "Angular e suas novidades",
-                    Local = "Curitiba",
-                    Lote = "3º Lote",
-                    QtdPessoas= 350,
-                    DataEvento = DateTime.Now.AddDays(5) .ToString("dd/MM/yyyy")
-                },
-                new Evento()
-                {
-                    EventoId = 3,
-                    Tema = ".net core",
-                    Local = "Rio de Janeiro",
-                    Lote = "2º Lote",
-                    QtdPessoas= 380,
-                    DataEvento = DateTime.Now.AddDays(5) .ToString("dd/MM/yyyy")
-                }
-            };
+        public ActionResult<IEnumerable<Evento>> Get () {
+            
+            return _context.Eventos.ToList();
+
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<Evento> Get(int id)
-        {
-            return new Evento[]
-            {
-                new Evento()
-                {
-                    EventoId = 1,
-                    Tema = "Angular e .NET Core",
-                    Local = "Belo Horizonte",
-                    Lote = "1º Lote",
-                    QtdPessoas= 250,
-                    DataEvento = DateTime.Now.AddDays(2) .ToString("dd/MM/yyyy")
-                },
-                new Evento()
-                {
-                    EventoId = 2,
-                    Tema = "Angular e suas novidades",
-                    Local = "Curitiba",
-                    Lote = "3º Lote",
-                    QtdPessoas= 350,
-                    DataEvento = DateTime.Now.AddDays(5) .ToString("dd/MM/yyyy")
-                },
-                new Evento()
-                {
-                    EventoId = 3,
-                    Tema = ".net core",
-                    Local = "Rio de Janeiro",
-                    Lote = "2º Lote",
-                    QtdPessoas= 380,
-                    DataEvento = DateTime.Now.AddDays(5) .ToString("dd/MM/yyyy")
-                }
-            }.FirstOrDefault( x => x.EventoId == id );
+        [HttpGet ("{id}")]
+        public ActionResult<Evento> Get (int id) {
+
+            return _context.Eventos.FirstOrDefault(x => x.EventoId == id);
+            
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        public void Post ([FromBody] string value) { }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        [HttpPut ("{id}")]
+        public void Put (int id, [FromBody] string value) { }
 
         // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        [HttpDelete ("{id}")]
+        public void Delete (int id) { }
     }
 }
